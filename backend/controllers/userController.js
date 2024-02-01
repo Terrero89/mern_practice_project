422bf15fbadc2b4c6ded562a0a4bcfdbb88698c8
Terrero89 <sergio-terrero@hotmail.com>
2024-02-01 16:40:24 -0500

    sign up jws token set

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,13 @@
-//userScheme
-const User = require("../models/userModel");
+const User = require("../models/userModel"); //userScheme
+
+const jwt = require("jsonwebtoken"); //jws token
+
+//custom function for token
+//screte key from env.
+//jwt sign --> 3 args, id arg, secrete key used, and how long it should last
+const createToken = (_id) => {
+  return jwt.sign({ _id: _id }, process.env.SECRET_KEY,{});
+};
 
 //login user controller
 const loginUser = async (req, res) => {
@@ -9,14 +17,17 @@ const loginUser = async (req, res) => {
 //signup user controller
 const signupUser = async (req, res) => {
   const { email, password } = req.body; // inputs from user body
+
   try {
     const user = await User.signup(email, password); //momngoose method for signup
-    res.status(200).json({email, user});
+    //? CREATE A TOKEN HERE.
+    const token = createToken(user._id);
 
+//instead of user, we pass to jws a token with hte token created id topass it to the browser
+    res.status(200).json({ email, token});
   } catch (error) {
-    res.status(400).json({error:error.message})
+    res.status(400).json({ error: error.message });
   }
-
 };
 
 module.exports = {
