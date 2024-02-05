b4f9ff3df9f23546513d2d36be3896d67cd287f1
Terrero89 <sergio-terrero@hotmail.com>
2024-02-05 12:50:35 -0500

    signup auth done

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,42 +6,42 @@ const jwt = require("jsonwebtoken"); //jws token
 //screte key from env.
 //jwt sign --> 3 args, id arg, secrete key used, and how long it should last
 const createToken = (_id) => {
-  return jwt.sign({ _id: _id }, process.env.SECRET_KEY,{});
+    return jwt.sign({_id: _id}, process.env.SECRET_KEY, {});
 };
 
 //login user controller
 const loginUser = async (req, res) => {
- const {email,password} =  req.body
+    const {email, password} = req.body
 
- try {
-    const user = await User.login(email, password); //momngoose method for login
-    //? CREATE A TOKEN HERE.
-    const token = createToken(user._id);
+    try {
+        const user = await User.login(email, password); //momngoose method for login
+        //? CREATE A TOKEN HERE.
+        const token = createToken(user._id);
 
 //instead of user, we pass to jws a token with hte token created id topass it to the browser
-    res.status(200).json({ email, token});
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+        res.status(200).json({email, token});
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
 };
 
 //signup user controller
 const signupUser = async (req, res) => {
-  const { email, password } = req.body; // inputs from user body
+    const {email, password} = req.body; // inputs from user body
 
-  try {
-    const user = await User.signup(email, password); //momngoose method for signup
-    //? CREATE A TOKEN HERE.
-    const token = createToken(user._id);
+    try {
+        const user = await User.signup(email, password); //momngoose method for signup
+        //? CREATE A TOKEN HERE.
+        const token = createToken(user._id);
 
 //instead of user, we pass to jws a token with hte token created id topass it to the browser
-    res.status(200).json({ email, token});
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+        res.status(200).json({email, token});
+    } catch (error) {
+        res.status(400).json({error: error.message});
+    }
 };
 
 module.exports = {
-  signupUser,
-  loginUser,
+    signupUser,
+    loginUser,
 };
--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -8,31 +8,33 @@ const getWorkoutsController = async (req, res) => {
 
 //?create new workout controller
 const createWorkoutController = async (req, res) => {
-  const {title, load, reps} = req.body
+  const { title, load, reps } = req.body;
 
-  let emptyFields = []
+  let emptyFields = [];
 
   if (!title) {
-    emptyFields.push('title')
+    emptyFields.push("title");
   }
   if (!load) {
-    emptyFields.push('load')
+    emptyFields.push("load");
   }
   if (!reps) {
-    emptyFields.push('reps')
+    emptyFields.push("reps");
   }
   if (emptyFields.length > 0) {
-    return res.status(400).json({ error: 'Please fill in all fields', emptyFields })
+    return res
+      .status(400)
+      .json({ error: "Please fill in all fields", emptyFields });
   }
 
   // add to the database
   try {
-    const workout = await Workout.create({ title, load, reps })
-    res.status(200).json(workout)
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    res.status(400).json({ error: error.message });
   }
-}
+};
 //get single workout controller
 
 const getWorkoutController = async (req, res) => {
@@ -53,44 +55,47 @@ const getWorkoutController = async (req, res) => {
 };
 
 const deleteWorkoutController = async (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({error: 'No such workout'})
+    return res.status(400).json({ error: "No such workout" });
   }
 
-  const workout = await Workout.findOneAndDelete({_id: id})
+  const workout = await Workout.findOneAndDelete({ _id: id });
 
-  if(!workout) {
-    return res.status(400).json({error: 'No such workout'})
+  if (!workout) {
+    return res.status(400).json({ error: "No such workout" });
   }
 
-  res.status(200).json(workout)
-}
+  res.status(200).json(workout);
+};
 
 // update a workout
 const updateWorkoutController = async (req, res) => {
-  const { id } = req.params
+  const { id } = req.params;
 
   if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({error: 'No such workout'})
+    return res.status(400).json({ error: "No such workout" });
   }
 
-  const workout = await Workout.findOneAndUpdate({_id: id}, {
-    ...req.body
-  })
+  const workout = await Workout.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
 
   if (!workout) {
-    return res.status(400).json({error: 'No such workout'})
+    return res.status(400).json({ error: "No such workout" });
   }
 
-  res.status(200).json(workout)
-}
+  res.status(200).json(workout);
+};
 //module exported as obeject to import each individual workout controller
 module.exports = {
   createWorkoutController,
   getWorkoutsController,
   getWorkoutController,
   deleteWorkoutController,
-  updateWorkoutController
+  updateWorkoutController,
 };
